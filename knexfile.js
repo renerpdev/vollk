97e4e9b990b3572328ca88c6f163748954174865
Rene Ricardo <i2rhop@gmail.com>
2018-06-01 16:39:20 -0400

    feat: setting new mode in the env variables

--- knexfile.js
+++ knexfile.js
@@ -5,25 +5,22 @@ dotenv.config()// setting the env variables
 
 module.exports = {
     development: {
-        client: process.env.DB_CLIENT || 'sqlite3',
-        connection: {
-            filename: process.env.SQLITE_PATH || path.resolve('db.sqlite')
-        }
-    },
-    production: {
         client: process.env.DB_CLIENT || 'mysql',
         connection: {
             host: process.env.DB_HOST || 'localhost',
             user: process.env.DB_USER || 'root',
             password: process.env.DB_PASS || '',
             database: process.env.DB_NAME || 'vollk'
-        },
-        // migrations: {
-        //     tableName: 'migrations',
-        //     directory: __dirname + '/db/migrations',
-        // },
-        // seeds: {
-        //     directory: __dirname + '/db/seeds'
-        // }
+        }
+    },
+    production: {
+        client: process.env.DB_CLIENT || 'pg',
+        connection: process.env.DB_URL || '',
+    },
+    local: {
+        client: process.env.DB_CLIENT || 'sqlite3',
+        connection: {
+            filename: "./db.sqlite"
+        }
     }
 }
